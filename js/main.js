84c578b4c1c72caf35e7307705adbf04245c4849
Filippo Pittau <filippo.pittau@gmail.com>
2020-02-17 16:58:15 +0100

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nome = prompt ('Nome');
+document.getElementById('nome').innerHTML = nome;
+console.log(nome);
+var cognome = prompt ('Cognome');
+// var colorePreferito = ('Colore preferito');
+document.getElementById('container').setAttribute('class' , 'visible');
